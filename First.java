cf6ade7f45839be3dbabcea42567ef6d1a20dfea
krishna14144 <53072332+krishna14144@users.noreply.github.com>
2019-08-09 14:07:14 +0530

    Add files via upload

---
+++ First.java
@@ -0,0 +1,14 @@
+
+public class First {
+
+public static void main ( String args[]) {
+
+System.out.println("hello world first time...");
+
+   for (int x = 2; x <= 4; x++) 
+    System.out.println("Value of x:" + x);
+
+
+
+}
+}
